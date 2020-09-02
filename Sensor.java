65051bed0e3d59322d223bf482334a847701e91e
rush-d <dixitrushikesh1999@gmail.com>
2020-09-02 17:54:54 +0530

    Initial commit : Motion Detection method

--- Sensor.java
+++ Sensor.java
@@ -3,8 +3,23 @@ package com.assignment;
 public class Sensor {
     int loc_w;
     int loc_l;
-    boolean status;
+
     public Sensor() {
+    	
+    }
+
+    public boolean sensorState = false;     //FALSE when Sensor is OFF and TRUE when ON
+    public boolean infiltratorState = false;    //FALSE when Infiltrator is standing and TRUE when moving
+
+    public boolean detectMovement (boolean newInfiltratorState) {
+        //Infiltrator gets caught
+        if(newInfiltratorState != infiltratorState && sensorState == true) return true;
+        
+        //Infiltrator doesn't get caught
+        infiltratorState = newInfiltratorState;
+        return false;
+    }
+
+    public void updateState() {
 
     }
-}
